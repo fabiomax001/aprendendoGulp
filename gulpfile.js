622f66845e995cb0e1a8ce0063821c92be686c16
Fabio <fabiomax001@gmail.com>
2019-04-21 17:57:28 -0300

    atualizando dependencias

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,17 @@ var watch = require('gulp-watch');
 // instalar: npm install --save-dev gulp-ruby-sass
 //task para o sass
 gulp.task('sass', function(){
-	return sass('sass/**/*.sass').pipe(gulp.dest('css'));
+	return sass('sass/*.sass').pipe(gulp.dest('css'));
 });
 
 //instalar: npm install gulp-watch --save-dev
 //task para o gulp-watch
 gulp.task('watch', function(){
-	gulp.watch('sass/**/*.sass',['sass']);	
+	watch('sass/**/*.sass',['sass']);	
 });
 
 //task defaut (padrão)
-gulp.task('default', ['sass','watch']);
+//gulp.task('default', ['sass', 'watch']);
+//gulp.task('default', function(){
+//	console.log('Ola mundo!');
+//});
