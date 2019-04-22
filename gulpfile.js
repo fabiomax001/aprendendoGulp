ed813632b596b05295057b4ca8310f8dd334bd2b
Fabio <fabiomax001@gmail.com>
2019-04-21 23:03:32 -0300

    ajustando o projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,19 @@
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 
-
-// instalar: npm install --save-dev gulp-ruby-sass
 //task para o sass
-gulp.task('sass', function(){
-	return sass('sass/*.sass').pipe(gulp.dest('css'));
+gulp.task('sass', function () {
+    return gulp.src('sass/*.sass')
+    //.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sass({outputStyle: 'normal'}).on('error', sass.logError))
+    .pipe(gulp.dest('css'));
 });
 
-//instalar: npm install gulp-watch --save-dev
-//task para o gulp-watch
-gulp.task('watch', function(){
-	watch('sass/**/*.sass',['sass']);	
+//task para o watch
+gulp.task('watch', function () {
+    gulp.watch('sass/*.sass', gulp.series('sass'));
 });
 
-//task defaut (padrão)
-//gulp.task('default', ['sass', 'watch']);
-//gulp.task('default', function(){
-//	console.log('Ola mundo!');
-//});
+//task default gulp
+gulp.task('default', gulp.series('sass','watch'));
